87e7472a2f729bb8a6d0e95d2b1c118d2207c4b8
yahyathegreat <yahyamardan1234@gmail.com>
2024-10-04 20:07:12 +0500

    reverse

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ sum=0
 for i in range(1, n+1):
     sum = sum+i
     print("\nsum =", sum)
+string = input("please enter your own string : ")
+string2 = ('')
+for i in string:
+    string2 = i + string2
+print("\nthe original string =", string)
+print("the reversed string = ", string2)
