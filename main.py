0267d95e57cf9a5e59293d0839d1320cc5a68529
yahyathegreat <yahyamardan1234@gmail.com>
2024-10-04 20:13:21 +0500

    reverse number

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ for i in string:
     string2 = i + string2
 print("\nthe original string =", string)
 print("the reversed string = ", string2)
+n = int(input("enter the value of n: "))
+print ("numbber from {0} to {1} are ". format(n,1))
+for i in range (n,0,-1):
+    print (i)
